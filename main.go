77fa7f556d327dd9dd6758bd56e18d43e1a4496d
oscar <oaangarita@yahoo.com.ar>
2025-06-19 21:24:33 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"database/sql"
-	"log"
-	"time"
 
-	_ "github.com/go-sql-driver/mysql"
-)
 
 func main() {
-	dns := "oscar:emi@tcp(localhost:3306)/loft"
-	db, err := sql.Open("mysql", dns)
-	if err != nil {
-		log.Fatal(err)
-	}
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Conexion exitosa")
+	Conexion()
 }
